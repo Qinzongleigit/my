26bbe3513d07f29aa70708acafb2b95826210a3d
覃宗雷 <qinzonglei@tanzongleideiMac.local>
2017-09-19 23:57:40 +0800

    Initial Commit

---
+++ gitdemotest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitdemotest
+//
+//  Created by 覃宗雷 on 2017/9/19.
+//  Copyright © 2017年 leizongqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitdemotest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitdemotest
+//
+//  Created by 覃宗雷 on 2017/9/19.
+//  Copyright © 2017年 leizongqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
